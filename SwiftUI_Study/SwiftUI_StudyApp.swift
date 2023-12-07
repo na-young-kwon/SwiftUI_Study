2c4091116ca00ac017e9de48bfc66909de2fcb19
na-young-kwon <kny0651@naver.com>
2023-12-07 22:56:09 +0900

    프로젝트 생성

---
+++ SwiftUI_Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI_Study
+//
+//  Created by 권나영 on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
